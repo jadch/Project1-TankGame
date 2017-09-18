2c2f2656a6124ba6b871c20a266f1061fab5934c
Jad ch <jad.chalita@gmail.com>
2017-09-18 17:31:17 +0200

    Players now have control over their tanks

--- main.js
+++ main.js
@@ -16,24 +16,32 @@ $(document).ready( function () {
 function play () {
   // This function will handle the whole game logic, executing at each requestAnimationFrame
 
-  player1.advance()
-  player2.advance()
-  player1.bulletAdvance(BULLET_SPEED_X, BULLET_SPEED_Y)
-  player2.bulletAdvance(BULLET_SPEED_X, BULLET_SPEED_Y)
+  // Player 1 moves
+  if (zPressed) player1.advance()
+  if (qPressed) player1.advance(0, 1)
+  if (dPressed) player1.advance(0, -1)
+
+  // Player 2 moves
+  if (oPressed) player2.advance()
+  if (kPressed) player2.advance(0, 1)
+  if (mPressed) player2.advance(0, -1)
 
   // Player 1 shoots
-  if (zPressed && performance.now() - p1_sinceLastShot > 500) {
+  if (aPressed && performance.now() - p1_sinceLastShot > 500) {
     let bullet_id = player1.shoot()
     p1_sinceLastShot = performance.now() // avoids the player shooting multiple times immediately
     $('#board').append('<div class="bullet" id="p1bullet' + bullet_id + '"></div>') // adding the bullet to the DOM
   }
 
   // Player 2 shoots
-  if (upPressed && performance.now() - p2_sinceLastShot > 500) {
+  if (iPressed && performance.now() - p2_sinceLastShot > 500) {
     let bullet_id = player2.shoot()
     p2_sinceLastShot = performance.now()
     $('#board').append('<div class="bullet" id="p2bullet' + bullet_id + '"></div>')
   }
+
+  player1.bulletAdvance(BULLET_SPEED_X, BULLET_SPEED_Y)
+  player2.bulletAdvance(BULLET_SPEED_X, BULLET_SPEED_Y)
 
   renderGame()
   requestAnimationFrame(play)
@@ -69,34 +77,41 @@ function renderGame () {
   document.addEventListener('keydown', KeyDownFunc, false)
   document.addEventListener('keyup', KeyUpFunc, false)
 
-  // Player 1 controls: A - Z - E
+  // Player 1 controls: QZD for movement, A for fire
   var aPressed = false
   var zPressed = false
-  var ePressed = false
+  var qPressed = false
+  var dPressed = false
   var p1_sinceLastShot = performance.now()
-  // Player 2 controls: left - up - right
-  var leftPressed = false
-  var upPressed = false
-  var rightPressed = false
+
+  // Player 2 controls: KOM for movement, I for fire
+  var iPressed = false
+  var kPressed = false
+  var oPressed = false
+  var mPressed = false
   var p2_sinceLastShot = performance.now()
 
 
   function KeyDownFunc (event) {
     if (event.keyCode === 65 ) aPressed = true
     if (event.keyCode === 90 ) zPressed = true
-    if (event.keyCode === 69 ) ePressed = true
-    if (event.keyCode === 37 ) leftPressed = true
-    if (event.keyCode === 38 ) upPressed = true
-    if (event.keyCode === 39 ) rightPressed = true
+    if (event.keyCode === 81 ) qPressed = true
+    if (event.keyCode === 68 ) dPressed = true
+    if (event.keyCode === 73 ) iPressed = true
+    if (event.keyCode === 75 ) kPressed = true
+    if (event.keyCode === 79 ) oPressed = true
+    if (event.keyCode === 77 ) mPressed = true
   }
 
   function KeyUpFunc (event) {
     if (event.keyCode === 65 ) aPressed = false
     if (event.keyCode === 90 ) zPressed = false
-    if (event.keyCode === 69 ) ePressed = false
-    if (event.keyCode === 37 ) leftPressed = false
-    if (event.keyCode === 38 ) upPressed = false
-    if (event.keyCode === 39 ) rightPressed = false
+    if (event.keyCode === 81 ) qPressed = false
+    if (event.keyCode === 68 ) dPressed = false
+    if (event.keyCode === 73 ) iPressed = false
+    if (event.keyCode === 75 ) kPressed = false
+    if (event.keyCode === 79 ) oPressed = false
+    if (event.keyCode === 77 ) mPressed = false
   }
 
 })
